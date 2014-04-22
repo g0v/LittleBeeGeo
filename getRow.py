4647f01ef91ea2988757a4ad7add45c7e8772591
Kirsten Liu <kirliu077@gmail.com>
2014-04-22 22:51:56 +0800

    getRow minor changes

--- getRow.py
+++ getRow.py
@@ -6,7 +6,8 @@ import gspread
 #https://pypi.python.org/pypi/gspread/0.1.0
 
 ##NOTICE::
-##the users need to login into the google spreadsheet first, and then provide their accounts and passwords here.
+##the users need to login into the google spreadsheet first,
+##and then provide their accounts and passwords here.
 
 ###remember:: delete your OWN account
 email = raw_input("please key in your gmail account: ")
@@ -15,7 +16,8 @@ password = raw_input("please key in your gmail account's password: ")
 print email , password
 
 
-###try if c = gspread.Client(auth=('user@example.com', 'qwertypassword')) needed to be login on g-spread first or not
+###try if c = gspread.Client(auth=('user@example.com', 'qwertypassword'))
+###needed to be login on g-spread first or not
 #gc = gspread.login(email, password)
 gc = gspread.Client(auth=(email, password))
 gc.login()
@@ -29,14 +31,18 @@ sheet = gc.open(sheetName_uni).sheet1
 
 #sheet = gc.open_by_url('https://docs.google.com/spreadsheets/d/18pLzqR1vJWWFR7lE9ZQnUViysOj-C_o6SVgsp1F3lOQ/')
 ##NOTICE::
-##open_by_url api is not working for the new version of google spreadsheet now, so please use opne by title api.
+##open_by_url api is not working for the new version of google spreadsheet now,
+##so please use opne by title api.
 
 row_num = 1
-values_list = sheet.row_values(row_num)
-print values_list
+rowValues_list = sheet.row_values(row_num)
+print rowValues_list
 
 row_amount = 1
 row_num = row_num + row_amount
-##and you can know how many column there is(col_count) or by the length of a row, and then next time you know how many rows you get (by head(cusor is moved) to get_all)
+
+col_num = length(rowValues_list)
+##and you can know how many column there is(col_count) or by the length of a row,
+##and then next time you know how many rows you get (by head(cusor is moved) to get_all)
 
 
