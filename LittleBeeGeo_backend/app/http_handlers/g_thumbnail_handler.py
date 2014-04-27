a08584e508210932357affd7291a0e12378d9012
chhsiao <hsiao.chuanheng@gmail.com>
2014-04-27 14:27:30 +0800

    get thumbnail_filename

--- LittleBeeGeo_backend/app/http_handlers/g_thumbnail_handler.py
+++ LittleBeeGeo_backend/app/http_handlers/g_thumbnail_handler.py
@@ -11,12 +11,12 @@ from app import cfg
 from app import util
 
 def g_thumbnail_handler(img_id):
-    db_result = util.db_find_one('bee_img', {'the_id': img_id}, {'_id': False, 'filename': True, 'content_type': True})
+    db_result = util.db_find_one('bee_img', {'the_id': img_id}, {'_id': False, 'thumbnail_filename': True, 'content_type': True})
 
     if not db_result:
         return ('image/gif', util.empty_img())
 
-    the_filename = db_result.get('filename', '')
+    the_filename = db_result.get('thumbnail_filename', '')
 
     full_filename = '/data/thumbnail/bee/' + the_filename
 
