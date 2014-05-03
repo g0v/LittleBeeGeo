a7b9596e6f0443825987b6734fdda339cb14a589
chhsiao <hsiao.chuanheng@gmail.com>
2014-05-03 09:39:18 +0800

    init add LittleBeeGeo_crawlers

--- scripts_op/init.sh
+++ scripts_op/init.sh
@@ -7,6 +7,13 @@ pip install -r requirements.txt
 cd ..
 deactivate
 
+cd LittleBeeGeo_crawlers
+virtualenv __
+. __/bin/activate
+pip install -r requirements.txt
+cd ..
+deactivate
+
 cd LittleBeeGeo-frontend
 ./scripts/init.sh
 cd ..
