ffa83263b405cc9de98c6fe6753380960e38b9f4
chhsiao <hsiao.chuanheng@gmail.com>
2014-05-20 00:27:50 +0800

    ad_version

--- LittleBeeGeo_crawlers/app/http_handlers/get_versions_handler.py
+++ LittleBeeGeo_crawlers/app/http_handlers/get_versions_handler.py
@@ -12,7 +12,7 @@ from app import util
 
 def get_versions_handler(params):
     n_db_result = util._int(params.get('n', 1))
-    db_result = util.db_find_it('bee_csv', {'is_processed_address': True, 'is_processed_versions': {'$ne': True}}, {'_id': False, 'csv_key': True, 'address': True, 'google_address': True, 'geo': True, 'version_text': True, 'versions': True})
+    db_result = util.db_find_it('bee_csv', {'is_processed_address': True, 'is_processed_ad_version': {'$ne': True}}, {'_id': False, 'csv_key': True, 'address': True, 'google_address': True, 'geo': True, 'version_text': True, 'versions': True})
     db_result = db_result.limit(n_db_result)
 
     db_result = list(db_result)
