6a58b2408c7c78936a2b5506f431f3bdb795343c
chhsiao <hsiao.chuanheng@gmail.com>
2014-05-19 08:12:39 +0800

    the_id in post_ad_version_handler

--- LittleBeeGeo_crawlers/app/http_handlers/post_ad_version_handler.py
+++ LittleBeeGeo_crawlers/app/http_handlers/post_ad_version_handler.py
@@ -23,5 +23,9 @@ def post_ad_version_handler(params):
 
     db_result = util.db_find_one('bee_csv', {"csv_key": csv_key}, {"_id": False, "town": True, "count": True, "deliver_time": True, "deliver_date": True, "save_time": True, "geo": True, "county": True, "address": True, "user_name": True, "is_processed_ad_version": True, "is_processed_address": True, "csv_key": True})
 
+    the_id = str(db_result.get('save_time', 0)) + "_" + util.uuid()
+
+    db_result['the_id'] = the_id
+
     if db_result.get('is_processed_ad_version', False) and db_result.get('is_processed_address', False):
         util.db_update('bee', {'csv_key': csv_key}, db_result)
