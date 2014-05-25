b876728a8e8658aeba92110c37f7a37f526e1e11
chhsiao <hsiao.chuanheng@gmail.com>
2014-05-26 01:31:36 +0800

    csv_key in post_all_ad_versions_handler

--- LittleBeeGeo_crawlers/app/http_handlers/post_all_ad_versions_handler.py
+++ LittleBeeGeo_crawlers/app/http_handlers/post_all_ad_versions_handler.py
@@ -13,4 +13,5 @@ from app import util
 def post_all_ad_versions_handler():
     db_results = util.db_find('bee_csv', {"is_processed_ad_version": True, "is_processed_address": True}, {"_id": False, "town": True, "count": True, "deliver_time": True, "deliver_date": True, "save_time": True, "geo": True, "county": True, "address": True, "user_name": True, "is_processed_ad_version": True, "is_processed_address": True, "csv_key": True, "ad_versions": True, "version_text": True, "memo": True, "deliver_status": True})
     for db_result in db_results:
+        csv_key = db_result.get('csv_key', '')
         util.db_update('bee', {'csv_key': csv_key}, db_result)
